d1c29ae42c89aa13cc7223430e50889025e0a68d
alexhDev2808 <colores.2808.8000@gmail.com>
2024-04-06 11:21:19 -0600

    Increase and decrease quantity

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,9 @@ function App() {
   const [data, setData] = useState(db);
   const [cart, setCart] = useState([]);
 
+  const MAX_ITEMS = 5;
+  const MIN_ITEMS = 1;
+
   function addToCart( item ) {
 
     const itemExists = cart.findIndex( guitar => guitar.id === item.id )
@@ -30,11 +33,44 @@ function App() {
     setCart( prevCart => prevCart.filter( guitar => guitar.id !== id ))
   }
 
+  function increaseQuantity( id ) {
+    const updatedCart = cart.map( item => {
+      if( item.id === id && item.quantity < MAX_ITEMS ){
+        return {
+          ...item,
+          quantity: item.quantity + 1
+        }
+      }
+
+      return item
+    })
+
+    setCart(updatedCart)
+  }
+
+  function decreaseQuantity( id ) {
+    const updatedCart = cart.map( item => {
+      if( item.id === id && item.quantity > MIN_ITEMS ){
+        return {
+          ...item,
+          quantity: item.quantity - 1
+        }
+      }
+
+      return updatedCart
+    })
+
+    setCart(updatedCart)
+
+  }
+
   return (
     <>
       <Header
         cart={cart}
         removeFromCart={removeFromCart}
+        increaseQuantity={increaseQuantity}
+        decreaseQuantity={decreaseQuantity}
       />
 
       <main className="container-xl mt-5">
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { useMemo } from "react";
 
-export default function Header({ cart, removeFromCart }) {
+export default function Header({ cart, removeFromCart, decreaseQuantity, increaseQuantity }) {
   // state derivado
   const isEmpty = useMemo( () => cart.length === 0, [cart] )
   const cartTotal = useMemo( () => cart.reduce((total, item) => total + (item.quantity * item.price), 0), [cart])
@@ -54,11 +54,17 @@ export default function Header({ cart, removeFromCart }) {
                             <td>{guitar.name}</td>
                             <td className="fw-bold">${guitar.price}</td>
                             <td className="flex align-items-start gap-4">
-                              <button type="button" className="btn btn-dark">
+                              <button
+                                onClick={ () => decreaseQuantity( guitar.id ) } 
+                                type="button" 
+                                className="btn btn-dark">
                                 -
                               </button>
                               {guitar.quantity}
-                              <button type="button" className="btn btn-dark">
+                              <button
+                                onClick={ () => increaseQuantity( guitar.id ) }  
+                                type="button" 
+                                className="btn btn-dark">
                                 +
                               </button>
                             </td>
